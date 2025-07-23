a6440fb701e72652bd644996ee08339d05ffef05
MagicGionson93 <gpasini93@gmail.com>
2025-07-23 11:39:48 +0200

    added the drink

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,15 @@
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
-import React from "react";
+import React, { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef();
+  // const videoTimelineRef = useRef()
+
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars, words" });
     const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
@@ -26,16 +32,36 @@ const Hero = () => {
       delay: 1,
     });
 
-    gsap.timeline({
+    gsap
+      .timeline({
         scrollTrigger: {
           trigger: "#hero",
           start: "top top",
           end: "bottom top",
           scrub: true,
-        }
+        },
       })
       .to(".right-leaf", { y: 200 }, 0)
       .to(".left-leaf", { y: -200 }, 0);
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top " : "bottom top";
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   }, []);
 
   return (
@@ -57,18 +83,29 @@ const Hero = () => {
           <div className="content">
             <div className="space-y-5 hidden md:block">
               <p>Cool. Crisp. Classic.</p>
-              <p className="subtitle">Sip the Spirit of Summer</p>
+              <p className="subtitle">Sip the Spirit <br /> of Summer</p>
             </div>
-            <div className="view-cocktails"></div>
-            <p className="subtitle">
-              Every cocktail on our menu is a blend of premium ingredients,
-              creative flair, and timeless recipes - designed to delight your
-              senses.
-            </p>
-            <a href="#cocktailes">View Cocktails</a>
+            <div className="view-cocktails">
+              <p className="subtitle">
+                Every cocktail on our menu is a blend of premium ingredients,
+                creative flair, and timeless recipes - designed to delight your
+                senses.
+              </p>
+              <a href="#cocktailes">View Cocktails</a>
+            </div>
           </div>
         </div>
       </section>
+
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+        />
+      </div>
     </>
   );
 };
